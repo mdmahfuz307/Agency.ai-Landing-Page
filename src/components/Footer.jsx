836982839c08851aa611f4b2fcc5bb62211ed274
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2026-02-12 04:14:55 +0600

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -89,7 +89,7 @@ const Footer = ({ theme }) => {
         viewport={{ once: true }}
         className="pb-6 text-sm text-gray-500 flex justify-center sm:justify-between gap-4 flex-wrap"
       >
-        <p>Copyright 2025 Agency- All Right Reserved</p>
+        <p>Copyright 2025 AxisTech.AI - All Right Reserved</p>
         <div className="flex items-center justify-between gap-4">
           <img src={assets.facebook_icon} alt="" />
           <img src={assets.twitter_icon} alt="" />
